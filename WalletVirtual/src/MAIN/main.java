c6a3418a991027f1335148aa0992dc653244525b
Tiago <Tiago@DESKTOP-4HGHI7C.fibertel.com.ar>
2024-10-27 15:17:15 -0300

    prueba

--- WalletVirtual/src/MAIN/main.java
+++ WalletVirtual/src/MAIN/main.java
@@ -9,19 +9,6 @@ import Modelo.Moneda;
 public class main {
 
     public static void main(String[] args) {
-        MonedaDAOImpl mon = new MonedaDAOImpl();
-
-        // Crear monedas
-        mon.crear(new Moneda("C", "Dogecoin", "DOGE", 3000.00, 99.00));
-        mon.crear(new Moneda("F", "Pesos Argentinos", "ARS", 1000.00, 50.00));
-        
-        // Listar monedas
-        List<Moneda> monedas = mon.listar();
-        for (Moneda moneda : monedas) {
-            System.out.println(moneda.getNombre() + " - " + moneda.getValorDolar());
-        }
-
-        // Cerrar la conexión
-        ConexionBD.closeConnection();
+       
     }
 }
