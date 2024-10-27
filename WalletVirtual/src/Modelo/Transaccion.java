01e2a163ca0e7d0c406d4a9b078538bf7bf245e1
Tiago <Tiago@DESKTOP-4HGHI7C.fibertel.com.ar>
2024-10-27 03:23:16 -0300

    agrego Transaccion

--- WalletVirtual/src/Modelo/Transaccion.java
+++ WalletVirtual/src/Modelo/Transaccion.java
@@ -1,5 +1,31 @@
 package Modelo;
 
-public class Transaccion {
+import java.time.LocalDateTime;
 
+public class Transaccion {
+    private String resumen;           // string con la data del resumen
+    private LocalDateTime fechaHora;  // FECHA
+    // Operacion
+    // Estado
+// Podriamos ver aca de si agregar los atributos que teniamos en el UML o respetar directamente lo que nos dieron ellos en la tabla
+    //En mi opinion no porque este modelo de Transaccion es re distinto al nuestro xdd
+    
+    // misma idea que Activo con el constructor
+
+    // Getters y setters
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
 }
